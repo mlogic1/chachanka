291d298c43a69077f42d20e8045d1f677b17353f
Filip Radić <multilogic@outlook.com>
2020-01-18 10:22:09 +0100

    initial commit
    - added basic main and vs solution

---
+++ Chachanka/src/Program.cs
@@ -0,0 +1,34 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace Chachanka
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private const string token = "";
+
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            Console.WriteLine("Chachanka");
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
