dcb4fc454fcfb608500aa3dd2dd254a55f02e156
mlogic1 <multilogic@outlook.com>
2024-11-22 15:44:07 +0100

    indent fixes

--- Chachanka/Model/GameDeals/Deal.cs
+++ Chachanka/Model/GameDeals/Deal.cs
@@ -8,24 +8,24 @@ namespace chachanka.Model.GameDeals
 {
 	public class Deal
 	{
-	public required string internalName { get; set; }
-	public required string title { get; set; }
-	public string? metacriticLink { get; set; }
-	public required string dealID { get; set; }
-	public required string storeID { get; set; }
-	public required string gameID { get; set; }
-	public required string salePrice { get; set; }
-	public required string normalPrice { get; set; }
-	public required string isOnSale { get; set; }
-	public required string savings { get; set; }
-	public required string metacriticScore { get; set; }
-	public string? steamRatingText { get; set; }
-	public required string steamRatingPercent { get; set; }
-	public required string steamRatingCount { get; set; }
-	public string? steamAppID { get; set; }
-	public required uint releaseDate { get; set; }
-	public required uint lastChange { get; set; }
-	public required string dealRating { get; set; }
-	public required string thumb { get; set; }
+		public required string internalName { get; set; }
+		public required string title { get; set; }
+		public string? metacriticLink { get; set; }
+		public required string dealID { get; set; }
+		public required string storeID { get; set; }
+		public required string gameID { get; set; }
+		public required string salePrice { get; set; }
+		public required string normalPrice { get; set; }
+		public required string isOnSale { get; set; }
+		public required string savings { get; set; }
+		public required string metacriticScore { get; set; }
+		public string? steamRatingText { get; set; }
+		public required string steamRatingPercent { get; set; }
+		public required string steamRatingCount { get; set; }
+		public string? steamAppID { get; set; }
+		public required uint releaseDate { get; set; }
+		public required uint lastChange { get; set; }
+		public required string dealRating { get; set; }
+		public required string thumb { get; set; }
 	}
 }
